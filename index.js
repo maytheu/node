dc4e0c2400865f4a9ce41ca71f1ea4bee6da2c20
maytheu <maytheu98@gmail.com>
2023-06-05 15:06:33 +0100

    streaming, parsing and filteing data from csv

---
+++ index.js
@@ -0,0 +1,29 @@
+const { createReadStream } = require("fs");
+const { parse } = require("csv-parse");
+
+const fileStream = [];
+
+//filter habitable planet
+const isHabitablePlanet = (planets) => {
+  return (
+    planets["koi_disposition"] == "CONFIRMED" &&
+    planets["koi_insol"] >= 0.36 &&
+    planets["koi_insol"] <= 1.11 &&
+    planets["koi_prad"] < 1.6
+  );
+};
+
+//reading file as stream
+createReadStream("keplar.csv")
+  .on("open", () => console.log("file opened"))
+  //format the readable stram s to array of object
+  .pipe(parse({ comment: "#", columns: true }))
+  .on("data", (data) => {
+    // console.log("streaming");
+    if (isHabitablePlanet(data)) fileStream.push(data);
+  })
+  .on("error", (err) => console.log(err, "err exist"))
+  .on("end", () => {
+    console.log(fileStream, `${fileStream.length} planets recorded`);
+    console.log("steam ended");
+  });
