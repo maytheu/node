ce79319d2bc4c9d91e894a3dba632df3a37505c2
maytheu <maytheu98@gmail.com>
2023-06-08 16:40:47 +0100

    setting up expressserver

---
+++ server/src/server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const app = require("./app");
+
+const PORT = 4005;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
