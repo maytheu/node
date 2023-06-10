c2b5944885be70437f7123aa8ae94b2343a0455f
maytheu <maytheu98@gmail.com>
2023-06-10 09:47:49 +0100

    morgan middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const morgan = require("morgan");
 
 const planetRouter = require("./routes/planets/planets.routes");
 
 const app = express();
 
 app.use(cors({ origin: "http://localhost:3000" }));
+
+app.use(morgan("combined"));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
