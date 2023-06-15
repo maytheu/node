929a4f7db8ff854018f3c7747509ea2543b454cd
maytheu <maytheu98@gmail.com>
2023-06-15 18:21:35 +0100

    adding swagger docs

--- server/src/routes/appv1.js
+++ server/src/routes/appv1.js
@@ -1,11 +1,23 @@
 const { Router } = require("express");
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs");
+const YAML = require("yaml");
+const path = require("path");
 
 const planetRouter = require("./planets/planets.routes");
 const launchesRouter = require("./launches/launches.router");
+
+const file = fs.readFileSync(
+  path.join(__dirname, "..", "..", "swagger.yaml"),
+  "utf8"
+);
+const swaggerDocument = YAML.parse(file);
 
 const api = Router();
 
 api.use("/planets", planetRouter);
 api.use("/launches", launchesRouter);
+api.use("/docs", swaggerUi.serve);
+api.get("/docs", swaggerUi.setup(swaggerDocument));
 
 module.exports = api;
