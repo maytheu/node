39f92e2f9dc67f86e6140aa1b3da69707f23db8b
maytheu <maytheu98@gmail.com>
2023-06-17 21:24:57 +0100

    github action

--- server/src/model/planets.schema.js
+++ server/src/model/planets.schema.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const planetsSchema = new mongoose.Schema({
-  kepler_name: { type: String, required: true },
+  kepler_name: { type: String, },
 });
 
 const planetModel = mongoose.model("Planet", planetsSchema);
